3ccf326ca94c689811a5eb9bc58a2b7717805fd0
creadicted <hello@creadicted.com>
2016-08-27 23:47:53 +0200

    Apply annotations to Customer.Name.

---
+++ src/VidlyCore/Data/Migrations/20160827214545_ApplyAnnotationsToCustomerName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VidlyCore.Data.Migrations
+{
+    public partial class ApplyAnnotationsToCustomerName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                maxLength: 255,
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                nullable: true);
+        }
+    }
+}
