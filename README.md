8795855fdad71c27d32f6953c599bbda49405a87
creadicted <hello@creadicted.com>
2016-08-27 23:42:47 +0200

    Updated Readme and Fixed Error with IDs in MembershipTypes Migrations file

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# vidlyCore
+## vidlyCore
 This is an .NET Core recreation of [Mosh Hamedanis](https://github.com/mosh-hamedani/) Vidly project.
-
 The Original version of this Project is hosted here: https://github.com/mosh-hamedani/vidly-mvc-5/ and is created during the [The Complete ASP.NET MVC 5 Course](https://www.udemy.com/the-complete-aspnet-mvc-5-course/learn/v4/overview)
 
 I do this to get used to the changes in .NET Core. 
+
+---
+
+### Setup migration
+With VS Package Manager
+
+```
+PM> cd .\src\VidlyCore
+PM> cd dotnet ef database update
+```
+
+---
+
+#### Useful links: 
+http://www.bricelam.net/2014/09/14/migrations-on-k.html
+http://benjii.me/2016/05/dotnet-ef-migrations-for-asp-net-core/
--- src/VidlyCore/Data/Migrations/20160824220624_PopulateMembershipTypes.cs
+++ src/VidlyCore/Data/Migrations/20160824220624_PopulateMembershipTypes.cs
@@ -8,15 +8,18 @@ namespace VidlyCore.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("INSERT INTO MembershipType (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (1, 0, 0, 0)");
-            migrationBuilder.Sql("INSERT INTO MembershipType (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (2, 30, 1, 10)");
-            migrationBuilder.Sql("INSERT INTO MembershipType (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (3, 90, 3, 15)");
-            migrationBuilder.Sql("INSERT INTO MembershipType (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (4, 300, 12, 20)");
+            migrationBuilder.Sql("INSERT INTO MembershipType (SignUpFee, DurationInMonths, DiscountRate) VALUES (0, 0, 0)");
+            migrationBuilder.Sql("INSERT INTO MembershipType (SignUpFee, DurationInMonths, DiscountRate) VALUES (30, 1, 10)");
+            migrationBuilder.Sql("INSERT INTO MembershipType (SignUpFee, DurationInMonths, DiscountRate) VALUES (90, 3, 15)");
+            migrationBuilder.Sql("INSERT INTO MembershipType (SignUpFee, DurationInMonths, DiscountRate) VALUES (300, 12, 20)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DELETE FROM MembershipType WHERE SignUpFee = 0 AND DurationInMonths = 0");
+            migrationBuilder.Sql("DELETE FROM MembershipType WHERE SignUpFee = 30 AND DurationInMonths = 1");
+            migrationBuilder.Sql("DELETE FROM MembershipType WHERE SignUpFee = 90 AND DurationInMonths = 3");
+            migrationBuilder.Sql("DELETE FROM MembershipType WHERE SignUpFee = 300 AND DurationInMonths = 12");
         }
     }
 }
